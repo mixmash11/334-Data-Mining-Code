a42c34432f6d70bbcbd777c4a5d448604ebce373
JH <jhhunt@g.cofc.edu>
2015-02-07 19:26:05 -0500

    Finished Item 3, Some progress on item 4

--- Neural Networks I.py
+++ Neural Networks I.py
@@ -19,18 +19,20 @@ def main():
 	df = readIntoDf(df)
 	# print df
 	
+	#Test 3
+	X = df.as_matrix(columns = df.columns[1:(np.size(df.columns)-1)])
+	w0 = 2 * np.random.random_sample(np.size(X,axis=1)) + -1
+	compute_y(w0,X[0,:])
+	f = (df.ix[:,np.size(df.columns)-1]-2)/2
+	f = f.as_matrix()
+	error(f[1],w0,X[0,])
 
+	#Test 4
+	f = (df[,ncol(df)]-2)/2
+	eta = 0.1
+	max_iter = 100
+	w = train_perceptron_sequential(X,f,max_iter,w0,eta)
 	
-
-def activation(y):
-	#Activation function
-	threshold = 0.0
-	
-	if y > threshold:
-		return 1
-	else:
-		return 0
-
 def readIntoDf( dfA ):
 	
 	for i in range( 1 , np.size( dfA.columns ) ):
@@ -43,6 +45,27 @@ def readIntoDf( dfA ):
 			dfA.ix[ nanRow, i ] = column.median()
 	
 	return dfA
+	
+def activation(y):
+	#Activation function
+	threshold = 0.0
+	
+	if y > threshold:
+		return 1
+	else:
+		return 0
+
+def compute_y( w , x ):
+	return np.dot( w ,  x )
+	
+def error(fy,w,x):
+	return fy - activation( compute_y( w , x ) )
+	
+
+	
+
+	
+
 	
 if __name__ == '__main__':
     main()
